5f13990a9039bf5fd396457c3bfe3224f8cb2f96
Rasalahmad <rasalahmadsit@gmail.com>
2021-09-24 00:11:45 +0600

    update site

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,20 +2,26 @@ import React from 'react';
 import './Cart.css';
 const Cart = (props) => {
     const {cart} = props;
-    const totalReducer = (previous, product) => previous + product.price;
-    const total = cart.reduce(totalReducer, 0);
-    /* let total = 0;
+    /* const totalReducer = (previous, product) => previous + product.price;
+    const total = cart.reduce(totalReducer, 0); */
+    let total = 0;
+    let totalQuantity = 0;
     for(const product of cart){
-        total = total + product.price;
-    }  */
-    const shipping = 15;
+        if(!product.quantity){
+            product.quantity = 1
+        }
+        // product.quantity = !product.quantity ? 1 : product.quantity
+        total = total + product.price * product.quantity;
+        totalQuantity = totalQuantity + product.quantity;
+    } 
+    const shipping = total > 0 ? 15 : 0;
     const tax = (shipping + total) / 10;
     const grandTotal = total + shipping + tax;
     return (
         <div className = 'cart-body'>
             <div className = 'cart-header'>
                 <h2>Order Summary</h2>
-                <p>Items Ordered: {props.cart.length}</p>
+                <p>Items Ordered: {totalQuantity}</p>
             </div>
           <div className = 'cart-calc'>
                 <div>
